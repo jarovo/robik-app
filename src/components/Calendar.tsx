3582509acf6abc0d72e80e92e2144ba2269a5dc7
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2025-03-05 07:45:11 +0100

    Add query and calendar selectors.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Stack from 'react-bootstrap/Stack';
 
 interface CalendarProps {
   token: string;
@@ -23,6 +22,8 @@ const Calendar: React.FC<CalendarProps> = ({token}) => {
   const [events, setEvents] = useState<Event[]>([]);
   const [dateSince, setDateSince] = useState<Date>(new Date())
   const [dateUntil, setDateUntil] = useState<Date>(new Date())
+  const [calendar, setCalendar] = useState('primary')
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const loadGapi = () => {
@@ -49,7 +50,8 @@ const Calendar: React.FC<CalendarProps> = ({token}) => {
     const listUpcomingEvents = async () => {
       try {
         const response = await window.gapi.client.calendar.events.list({
-          calendarId: 'primary',
+          calendarId: calendar,
+          q: query,
           timeMin: dateSince.toISOString(),
           timeMax: dateUntil.toISOString(),
           showDeleted: false,
@@ -67,23 +69,25 @@ const Calendar: React.FC<CalendarProps> = ({token}) => {
     };
 
     loadGapi();
-  }, [token, dateSince, dateUntil]);
+  }, [token, dateSince, dateUntil, calendar, query]);
 
   return (
     <div>
       <h2>Events</h2>
         <InputGroup>
+          <label>Calendar<input name="calendar" type="text" onChange={e => setCalendar(e.target.value)} defaultValue={calendar}/></label>
+          <label>Query<input name="query" type="text" onChange={e => setQuery(e.target.value)} defaultValue={query}/></label>
           <label>Date since <input name="date since" type="date" onChange={e => setDateSince(new Date(e.target.value))}/></label> 
           <label>Date until <input name="date until" type="date" onChange={e => setDateUntil(new Date(e.target.value))}/></label>
         </InputGroup>
         <div>
           {events.map((event) => (
             <>
-                <Form.Label>{(new Date(event.start.dateTime).toLocaleString())
-                  +  ' - ' + (new Date(event.end.dateTime).toLocaleString())}</Form.Label>
-
                 <InputGroup className="mb-3">                  
                   <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+                  <InputGroup.Text>{
+                  (new Date(event.start.dateTime).toLocaleString()) + ' - ' + (new Date(event.end.dateTime).toLocaleString())}
+                  </InputGroup.Text>
                     <Form.Control aria-label="Apperance in invoice" defaultValue={event.summary} />
                 </InputGroup>
             </>
