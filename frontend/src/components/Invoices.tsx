52f41e21a5ca9f466a6a1fb8fdc8589e3b8b68d6
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2025-03-14 15:21:49 +0100

    Use Form.Control for input with dataoptions.

--- frontend/src/components/Invoices.tsx
+++ frontend/src/components/Invoices.tsx
@@ -1,13 +1,11 @@
-import React, { PropsWithChildren, useState, ChangeEventHandler, use, useEffect, MouseEventHandler, ReactElement } from "react";
+import React, { PropsWithChildren, useState, ChangeEventHandler, use, useEffect, MouseEventHandler, ReactElement, FormEventHandler, FormEvent, ReactEventHandler, ChangeEvent } from "react";
 import { Api, Namespace, Invoice } from "fakturoid-js"
 
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
-import { CalendarEvents } from "./CalendarEvents";
 import { GoogleEvent } from "./Calendar";
-import { FloatingLabel, FormLabel, InputGroup } from "react-bootstrap";
-import InputGroupText from "react-bootstrap/esm/InputGroupText";
+import { InputGroup } from "react-bootstrap";
 import { Subject } from "fakturoid-js/lib/Api/ApiEntity";
 
 interface InvoicesProps {
@@ -15,51 +13,71 @@ interface InvoicesProps {
   CalendarEvents: ReactElement
 }
 
+
 const Invoices: React.FC<PropsWithChildren<InvoicesProps>> =({accessToken, CalendarEvents}) => {
 
-	const fakturoid = new Api({
-		namespace: Namespace.development,
-		credentials: { access_token: accessToken },
-		slug: "jaroslavhenner"
-	})
+	const [fakturoid, setFakturoid] = useState<Api|null>(null)
 	const [subjectName, setSubjectName] = useState("")
-	const [subjects, setSubjects] = useState<Subject[]>([])
+	const [subjectsOptions, setSubjectsOptions] = useState<Subject[]>([])
 	const [invoices, setInvoices] = useState<Invoice[]>([])
 	const [selectedSubject, setSelectedSubject] = useState<Number|null>(null)
 	const [events, setEvents] = useState<GoogleEvent[]>([])
 
-	useEffect(() => {
-		fakturoid.subjects.search({query: subjectName, page:0}).then(data => setSubjects(data))
-	}, [])
+	const onFakturoidSlugChange: ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFakturoid(new Api({
+			namespace: Namespace.development,
+			credentials: { access_token: accessToken },
+			slug: e.currentTarget.value
+		}))
+			
+		fakturoid?.subjects.search({query: subjectName, page: 1}).then(data => setSubjectsOptions(data)).catch(e => console.debug(`Unable to find subjects starting with ${subjectName}`, e))
+	}
 
 	const onSubjectSearch: ChangeEventHandler<HTMLInputElement> = (e)  => {
-		console.log("Searching for", e.target.value)
-		fakturoid.subjects.search({query: e.target.value, page: 1}).then(data => setSubjects(data))
+		fakturoid?.subjects.search({query: e.target.value, page: 1}).then(data => setSubjectsOptions(data))
 	}
 
 	const onSubjectSelect: ChangeEventHandler<HTMLInputElement> = (e) => {
-		console.log(e)
-		const selectedId= Number(subjects[0].id)
-		if(!selectedId) return
+		if (subjectsOptions.length === 0) return
+
+		const selectedId = Number(subjectsOptions[0]!.id)
 		setSelectedSubject(selectedId)
-		fakturoid.invoices.list({subject_id: selectedId, page: 1}).then(data => setInvoices(data))
+		fakturoid?.invoices.list({subject_id: selectedId, page: 1}).then(data => setInvoices(data))
+	}
+
+	const onInvoiceSelect: ChangeEventHandler<HTMLSelectElement> = (e: ChangeEvent) => {
+		if (subjectsOptions.length === 0) return
+		
+		const selectedId = Number(subjectsOptions[0].id)
+		if(!selectedId) return
+
+		setSelectedSubject(selectedId)
+
+		fakturoid?.invoices.list({subject_id: selectedId, page: 1}).then(data => setInvoices(data))
+	}
+
+	const fakturoidUpdate = (formData: FormData) => {
+		console.log("Form data", formData)
 	}
 
 	return (
 		<div>
 			<h2>Invoice</h2>
-			<Form>
+			<Form action={fakturoidUpdate}>
 				<InputGroup className="mb-3">
+					<InputGroup.Text>Fakturoid slug</InputGroup.Text>
+					<Form.Control onChange={onFakturoidSlugChange} aria-label="Fakturoid slug" />
+
 					<InputGroup.Text>Subject name</InputGroup.Text>
-					<input type="text" list="subjects" onChange={onSubjectSearch} onSelect={onSubjectSelect}/>
+					<Form.Control name="subjectName" type="text" list="subjects" onChange={onSubjectSearch} onSelect={onSubjectSelect}/>
 					<datalist id="subjects">
-						{subjects.map((subj) => (
+						{subjectsOptions.map((subj) => (
 						<option key={subj.id.toString()} value={subj.name}>{subj.name}</option>
 						))}
 					</datalist>
 
 					<InputGroup.Text>Invoice number</InputGroup.Text>
-					<select className="form-select" aria-label="Invoices">
+					<select name="invoiceNumber" className="form-select" aria-label="Invoices" onChange={onInvoiceSelect}>
 						{invoices.map((invoice) => (
 						<option key={invoice.id.toString()} value={invoice.number.toString()}>{invoice.number}</option>
 						))}
@@ -72,7 +90,7 @@ const Invoices: React.FC<PropsWithChildren<InvoicesProps>> =({accessToken, Calen
 					<InputGroup.Text>/hour</InputGroup.Text>
 				</InputGroup>
 
-				<Button variant="primary">Update Fakturoid</Button>
+				<Button variant="primary" type="submit">Submit</Button>
 
 				{CalendarEvents}
 			</Form>
